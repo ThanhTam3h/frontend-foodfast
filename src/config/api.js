5f3c6758adb96500ec877750b80492d3add5f2f2
ThanhTam3h <dao.thi.thanh.tam.0807@gmail.com>
2025-11-24 20:11:20 +0700

    fix: api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:3000/api/v1";
+export const API_URL =
+	process.env.REACT_APP_API_URL || "http://localhost:3000/api/v1";
 
 export const api = axios.create({
-  baseURL: API_URL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
+	baseURL: API_URL,
+	withCredentials: true,
+	headers: {
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
 });
